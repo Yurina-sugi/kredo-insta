f5d651d78514a512c9ff817bfc9b33b5ad247ff5
Yurina-sugi <yurina.pc253su@gmail.com>
2025-07-17 12:52:58 +0900

    updating ui

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,26 +9,36 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Post extends Model
 {
     use SoftDeletes;
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class)->withTrashed();
     }
 
-    public function categoryPost() {
+    public function categoryPost()
+    {
         return $this->hasMany(CategoryPost::class);
     }
 
     #To get all the comments of a post
-    public function comments() {
+    public function comments()
+    {
         return $this->hasMany(Comment::class);
     }
 
     #To get the likes of a post
-    public function likes() {
+    public function likes()
+    {
         return $this->hasMany(Like::class);
     }
 
     #Returns true if the Auth user already liked the post
-    public function isLiked() {
+    public function isLiked()
+    {
         return $this->likes()->where('user_id', Auth::user()->id)->exists();
+    }
+
+    public function likedUsers()
+    {
+        return $this->belongsToMany(User::class, 'likes');
     }
 }
